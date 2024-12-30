1330da3f9287d086952515128667da9044ea1d65
mhtwebdev <farzanakhtarf99@gmail.com>
2024-12-30 18:13:37 +0700

    test fix pubpres 14okt

--- resources/views/program-kerja/akademis/pubpres/14102024.blade.php
+++ resources/views/program-kerja/akademis/pubpres/14102024.blade.php
@@ -3,30 +3,30 @@
         <span class="font-bold">14 Oktober 2024:</span> IAChO (International Applied Chemistry Olympiad) 2024 & IAMO (International Applied Mathematics Olympiad) 2024
     </x-slot>
         <x-slot:imgcol1>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5094.jpg' heading="M. Azka Ghulam Idrus" caption="Grand Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5026.jpg' heading="Queen Tabriz Tsabita" caption="Gold Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5094.JPG' heading="M. Azka Ghulam Idrus" caption="Grand Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5026.JPG' heading="Queen Tabriz Tsabita" caption="Gold Award - IAChO 2024"/>
             <x-proker.image imglink='/images/prestasi/14okt/20241015_132152.jpg' heading="Alfitri Celesta Damanik" caption="Silver Award - IAChO 2024"/>
             <x-proker.image imglink='/images/prestasi/14okt/IMG-20241015-WA0005.jpg' heading="Amelia Febriani Rachman" caption="Silver Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5035.jpg' heading="Anindya Zilka Qisthi" caption="Silver Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5037.jpg' heading="Abigail Vipasyana Bombong" caption="Silver Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5040.jpg' heading="Kimberly Ciputri" caption="Silver Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5096.jpg' heading="M. Danendra Kuntoadji" caption="Silver Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5042.jpg' heading="Mutiara Alya Putri" caption="Bronze Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5043.jpg' heading="Adelia Nasywa Ramadhani" caption="Bronze Award - IAChO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5046.jpg' heading="Abida Nasywa Widjaya" caption="Bronze Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5035.JPG' heading="Anindya Zilka Qisthi" caption="Silver Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5037.JPG' heading="Abigail Vipasyana Bombong" caption="Silver Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5040.JPG' heading="Kimberly Ciputri" caption="Silver Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5096.JPG' heading="M. Danendra Kuntoadji" caption="Silver Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5042.JPG' heading="Mutiara Alya Putri" caption="Bronze Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5043.JPG' heading="Adelia Nasywa Ramadhani" caption="Bronze Award - IAChO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5046.JPG' heading="Abida Nasywa Widjaya" caption="Bronze Award - IAChO 2024"/>
         </x-slot>
         <x-slot:imgcol2>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5054.jpg' heading="Kevin Mangara Doloksaribu" caption="Grand Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5057.jpg' heading="Razka Ananda Setiawan" caption="Gold Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5066.jpg' heading="Fauzan Adiwidya Kurniawan" caption="Gold Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5077.jpg' heading="M. Naufal Hafiz" caption="Gold Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5080.jpg' heading="Carra Putri Ghaisani" caption="Gold Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5068.jpg' heading="Safa Alzena Riszy" caption="Silver Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5070.jpg' heading="Arva Mochammad Raditia" caption="Silver Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5088.jpg' heading="Muthohharuna Azzikra" caption="Silver Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5092.jpg' heading="Khadijah Naila Ibtisam" caption="Silver Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5084.jpg' heading="Syakira Basalama" caption="Bronze Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5073.jpg' heading="Taqiy Fathir Akbar" caption="Bronze Award - IAMO 2024"/>
-            <x-proker.image imglink='/images/prestasi/14okt/637A5095.jpg' heading="Victorius Almon Rapmalo Purba" caption="Bronze Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5054.JPG' heading="Kevin Mangara Doloksaribu" caption="Grand Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5057.JPG' heading="Razka Ananda Setiawan" caption="Gold Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5066.JPG' heading="Fauzan Adiwidya Kurniawan" caption="Gold Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5077.JPG' heading="M. Naufal Hafiz" caption="Gold Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5080.JPG' heading="Carra Putri Ghaisani" caption="Gold Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5068.JPG' heading="Safa Alzena Riszy" caption="Silver Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5070.JPG' heading="Arva Mochammad Raditia" caption="Silver Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5088.JPG' heading="Muthohharuna Azzikra" caption="Silver Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5092.JPG' heading="Khadijah Naila Ibtisam" caption="Silver Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5084.JPG' heading="Syakira Basalama" caption="Bronze Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5073.JPG' heading="Taqiy Fathir Akbar" caption="Bronze Award - IAMO 2024"/>
+            <x-proker.image imglink='/images/prestasi/14okt/637A5095.JPG' heading="Victorius Almon Rapmalo Purba" caption="Bronze Award - IAMO 2024"/>
         </x-slot>
 </x-proker.main>
