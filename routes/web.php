36941fde94e36b86a576b6d2a02b42c8762356d9
yeahbois2702 <88705858+yeahbois@users.noreply.github.com>
2025-10-20 11:11:29 +0700

    Update queue route response to 'Queue Closed'
    Change the response for the '/pudobooth/queue' route to indicate that the queue is closed.

--- routes/web.php
+++ routes/web.php
@@ -98,7 +98,7 @@ Route::get('/pudobooth', function () {
     return view('pudobooth.camera');
 });
 Route::get('/pudobooth/queue', function () {
-    return view('pudobooth.queue');
+    return "Queue Closed";
 });
 Route::get('/admin/pudobooth', function () {
     return view('pudobooth.admin');
